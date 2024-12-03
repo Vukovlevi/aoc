736d5ac0928e5ca4716910414ddabc5c9c1cd764
Szabó-Vukov Levente <szabo.vukov.levente@gmail.com>
2024-12-03 17:10:25 +0100

    feat: day2

--- day3/main.go
+++ day3/main.go
@@ -3,71 +3,35 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
-	"unicode"
 )
 
 func main() {
     data, _ := os.ReadFile("input.txt")
     str := string(data)
     sum := 0
-
-    index := 0
-    for index != -1 && index < len(str) {
-        index = strings.Index(str, "mul(")
-        if index == -1 {
-            continue
-        }
-
-        end := index + 4 + 8
-        if end > len(str) {
-            end = len(str)
-        }
-        num, i := parseMul(str[index + 4:end])
-        if num != -1 {
-            sum += num
-        }
-        str = str[index + 4 + i:]
+    dos := strings.Split(str, "do()")
+    for i, do := range dos {
+        dos[i] = strings.Split(do, "don't()")[0]
     }
 
+    for _, exec := range dos {
+        sum += parseMul(exec)
+    }
 
     fmt.Printf("the sum of the muls: %d\n", sum)
 }
 
-func parseMul(data string) (int, int) {
-    num1Str := ""
-    i := 0
-    for i < 3 && unicode.IsDigit(rune(data[i])) {
-        num1Str += string(data[i])
-        i++
+func parseMul(data string) int {
+    re := regexp.MustCompile(`mul\(([0-9]{1,3}),([0-9]{1,3})\)`)
+    matches := re.FindAllStringSubmatch(data, -1)
+    sum := 0
+    for _, match := range matches {
+        num1, _ := strconv.Atoi(match[1])
+        num2, _ := strconv.Atoi(match[2])
+        sum += num1 * num2
     }
-
-    if data[i] != ',' {
-        return -1, i + 1
-    }
-
-    i++
-    j := i + 3
-
-    num2Str := ""
-    for i < j && unicode.IsDigit(rune(data[i])) {
-        num2Str += string(data[i])
-        i++
-    }
-
-    if data[i] != ')' {
-        return -1, i + 1
-    }
-
-    num1, err := strconv.Atoi(num1Str)
-    if err != nil {
-        return -1, i + 1
-    }
-    num2, err := strconv.Atoi(num2Str)
-    if err != nil {
-        return -1, i + 1
-    }
-
-    return num1 * num2, i + 1
+    return sum
 }
